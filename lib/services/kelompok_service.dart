bc93f9ab24c48db4437b7c2590cf574d54eb43d9
VincentiusCS <vincenttiuschristian@gmail.com>
2026-03-12 21:34:59 +0700

    second update

--- lib/services/kelompok_service.dart
+++ lib/services/kelompok_service.dart
@@ -9,12 +9,10 @@ class KelompokService {
   /// Mengembalikan data kelompok (simulasi sumber data / repository)
   Kelompok getKelompok() {
     return const Kelompok(
-      namaKelompok: 'Kelompok 1 – Teknik Informatika',
+      namaKelompok: 'Teknologi Pemrograman Mobile IF D – Teknik Informatika',
       anggota: [
-        Anggota(nama: 'Andi Pratama', nim: '2301001', prodi: 'Teknik Informatika'),
-        Anggota(nama: 'Budi Santoso', nim: '2301002', prodi: 'Teknik Informatika'),
-        Anggota(nama: 'Citra Dewi',   nim: '2301003', prodi: 'Teknik Informatika'),
-        Anggota(nama: 'Dian Rahayu',  nim: '2301004', prodi: 'Teknik Informatika'),
+        Anggota(nama: 'Wahyu Febriansyah', nim: '123220033', prodi: 'Teknik Informatika'),
+        Anggota(nama: 'Vincentius Christian Sugianto', nim: '123220144', prodi: 'Teknik Informatika'),
       ],
     );
   }
